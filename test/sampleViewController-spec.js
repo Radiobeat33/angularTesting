74e3c8b24f4c33480eed006b8e83e9c4fdaf3dbd
Steven Williams <Steven30135@gmail.com>
2016-02-11 19:31:42 -0800

    Added test.html to view results

--- test/sampleViewController-spec.js
+++ test/sampleViewController-spec.js
@@ -17,6 +17,40 @@ describe('sampleViewController', function(){
         it('should read the testScope as hello',  function(){
             expect(scope.testScope).toEqual('hello');
         })
-    })
+    });
+
+    describe('multiplication test', function(){
+        it('should return the correct product',  function(){
+            scope.currentNum = 5;
+            scope.multiplier5(scope.currentNum);
+            expect(scope.currentResult).toEqual(25);
+        })
+    });
+
+    describe('multiplication test with incorrect value type', function(){
+        it('should return nothing',  function(){
+            scope.currentNum = 'A';
+            scope.multiplier5(scope.currentNum);
+            expect(scope.currentResult).toBeFalsy();
+        })
+    });
+
+    describe('multiplication test with no value', function(){
+        it('should return nothing',  function(){
+            scope.currentNum = '';
+            scope.multiplier5(scope.currentNum);
+            expect(scope.currentResult).toBeFalsy();
+        })
+    });
+
+    describe('multiplication test with 0 as the number', function(){
+        it('should return 0',  function(){
+            scope.currentNum = 0;
+            scope.multiplier5(scope.currentNum);
+            expect(scope.currentResult).toEqual(0);
+        })
+    });
 
 });
+
+
